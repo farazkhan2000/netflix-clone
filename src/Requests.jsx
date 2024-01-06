02e44df8cccc93bbf672faa16f755cbc9a173b55
Faraz khan <faraz_khan@nextpak.org>
2024-01-06 16:12:56 +0500

    Navbar and Main component added

---
+++ src/Requests.jsx
@@ -0,0 +1,10 @@
+const key = '2c2e97f660b4c29aae777e7e00b462ea';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
