3b694c666a0c998334993b2961c537da7a0531f4
Faraz khan <faraz_khan@nextpak.org>
2024-01-06 16:47:35 +0500

    Row component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import Main from '../components/Main';
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row title="Up Coming" fetchUrl={requests.requestUpcoming} />
+      <Row title="Popular" fetchUrl={requests.requestPopular} />
+      <Row title="Trending" fetchUrl={requests.requestTrending} />
+      <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
     </>
   )
 }
